c91eed45ff7f0b55057a163e6a7beb7073f351a4
rubenamador <ruben.amador.madrid@gmail.com>
2021-01-30 00:42:07 +0100

    Creacion de vistas iniciales para cada aplicacion

--- apps/Nodes/views.py
+++ apps/Nodes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def main_page(request):
+	return HttpResponse("Pagina Principal Aplicacion Nodes")
+	
+def index(request):
+	return HttpResponse("Indice Aplicacion Nodes")
