fafdc8689b17a53bcc7c87b42c6a80783c5c61ab
rubenamador <ruben.amador.madrid@gmail.com>
2021-01-30 00:46:14 +0100

    Creacion de plantillas o templates iniciales

--- apps/Links/views.py
+++ apps/Links/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def main_page(request):
-	return HttpResponse("Pagina Principal Aplicacion Links")
+	#return HttpResponse("Pagina Principal Aplicacion Links")
+	return render(request, 'links/index.html')
 	
 def index(request):
-	return HttpResponse("Indice Aplicacion Links")
+	#return HttpResponse("Indice Aplicacion Links")
+	return render(request, 'links/index.html')
--- apps/Nodes/views.py
+++ apps/Nodes/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def main_page(request):
-	return HttpResponse("Pagina Principal Aplicacion Nodes")
+	#return HttpResponse("Pagina Principal Aplicacion Nodes")
+	return render(request, 'nodes/index.html')
 	
 def index(request):
-	return HttpResponse("Indice Aplicacion Nodes")
+	#return HttpResponse("Indice Aplicacion Nodes")
+	return render(request, 'nodes/index.html')
